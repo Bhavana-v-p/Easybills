9ef257ab8d7e587f47b6734332d1b9c6770b41f1
Bhavana-v-p <bhavanavrindavan@gmail.com>
2025-12-12 08:36:27 +0530

    .

--- config/firebase.js
+++ config/firebase.js
@@ -16,8 +16,8 @@ try {
     private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
     
     // THE FIX: Properly format the private key (handles \n characters)
-    private_key: process.env.FIREBASE_PRIVATE_KEY 
-      ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') 
+    private_key: process.env.FIREBASE_PRIVATE_KEY
+      ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
       : undefined,
       
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
